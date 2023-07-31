e22533614bafb5d015c98dc3f787b0c6e6aa2c62
skrajput01 <skrajput01@hotmail.com>
2023-07-31 15:04:37 +0100

    Updated to include classes

--- dogs_life_p1/java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
+++ dogs_life_p1/java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepository.java
@@ -1,4 +1,5 @@
 package com.db.grad.javaapi.repository;
 
+import com.db.grad.javaapi.model.Dog;
 public interface DogsRepository {
 }
--- dogs_life_p1/java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
+++ dogs_life_p1/java-api/src/main/java/com/db/grad/javaapi/repository/DogsRepositoryStub.java
@@ -1,4 +1,14 @@
 package com.db.grad.javaapi.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public class DogsRepositoryStub implements DogsRepository {
+
+}
+
+
 public class DogsRepositoryStub {
 }
